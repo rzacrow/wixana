4332082e429e39ea6be525655a16c5d17a142c3b
Reza Abedini <rezacrow88@gmail.com>
2024-01-03 13:49:21 +0330

    remove search team

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .views import SignupDiscord, Signup, Dashboard, Login, Logout, CreateTeam, 
 urlpatterns = [
     path('signup/', Signup.as_view(), name='signup'),
     path('signup/discord/response/', SignupDiscord.as_view(), name='discord_response'),
-    path('login/', Login.as_view(), name='login'),
+    path('', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
 
